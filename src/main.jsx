6fddf0664775f55903decb35c31f82b99d7b0694
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 10:48:52 -0300

    michael jackson

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+//importando rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+
+//criando elementos
+
+const router = createBrowserRouter([
+  {
+    //elemento pai
+    path:'/', element:<App/>,
+    errorElement:<Error/>,
+
+    //elemento filho 
+
+    children:[
+      {path:'/',element:<Home/>},
+      {path:'/Produtos',element:<Produtos/>}
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/routes/Produtos.jsx
@@ -0,0 +1,11 @@
+const Produtos =()=>{
+    return(
+        <>
+        <h1>
+            Produtos
+        </h1>
+        
+        </>
+    )
+}
+export default Produtos
